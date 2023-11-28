e6938cec69f9c705468ba44e129c035973af05a7
AmitGujar <amithero3342@gmail.com>
2023-11-28 15:39:09 +0530

    script modified

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 ansible-playbook playbooks/vmplaybook.yaml $1
 echo "executing the config task"
 if [ $? -eq 0 ]; then
-    ansible-playbook playbooks/configplay.yml
+   # ansible-playbook playbooks/configplay.yml
     echo "it's good"
 else
     echo "something went wrong"
